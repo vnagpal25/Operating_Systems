2a622ab2df38f3f07636370d9378ff06fed4c7e2
Vansh Nagpal <you@example.com>
2023-04-03 13:52:56 -0400

    linting/documentation

--- project3/inc/shm_parent.h
+++ project3/inc/shm_parent.h
@@ -1,30 +1,40 @@
-#ifndef _SHM_PARENT_H_
-#define _SHM_PARENT_H_
+/*Copyright 2023 CSCE311 Project 3
+ * author - vnagpal
+ * Contains definition of abstract shared memory parent class
+ */
+#ifndef PROJECT3_INC_SHM_PARENT_H_
+#define PROJECT3_INC_SHM_PARENT_H_
 
-#define SHM_NAME "CSV_SHM"
-#define PROD_SEM "PRODUCER_SEMAPHORE"
-#define CONS_SEM "CONSUMER_SEMAPHORE"
-#define BUF_SIZE 1024     // max length of shared memory segment
+#define SHM_NAME "CSV_SHM"             // name of shared mem segment
+#define PROD_SEM "PRODUCER_SEMAPHORE"  // name of server semaphore
+#define CONS_SEM "CONSUMER_SEMAPHORE"  // name of client semaphore
+#define BUF_SIZE 1024                  // max length of shared memory segment
 #define MAX_PATH_SIZE 32  // safely assuming max size of file path as 32 bytes
-#define THREAD_NUM 4      // number of threads client side
-#include <fcntl.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <unistd.h>
+#define THREAD_NUM 4      // number of threads on client side
+#include <fcntl.h>        // O-flags for semaphores and shared memory
+#include <pthread.h>      // all thread functionality
+#include <semaphore.h>    // all named semaphore functionality
+#include <sys/mman.h>     // shared memory functionality and page flag constants
+#include <unistd.h>       // for truncating/closing shared memory
+
 class SharedMemoryParent {
  protected:
-  // struct which will be used to map in shared memory
+  // struct which will be used to map in shared memory on both server and client
+  // side
   struct shm_buf_ {
     char file_path[MAX_PATH_SIZE];
     size_t path_length;
     char file_lines[THREAD_NUM][BUF_SIZE];
-    size_t num_lines;  // number of lines sent over
   };
   int shm_fd_;           // file descriptor for shared memory segment
   sem_t* prod_sem_ptr_;  // pointer to producer semaphore
   sem_t* cons_sem_ptr_;  // pointer to consumer semaphore
+
+  /**
+   * Function is meant to call down on its own semaphore, and up on the opposite
+   * semaphore. Virtual because will be implemented by client and server in
+   * opposite fashions
+   */
   virtual void Sleep() = 0;
 };
-#endif  // _SHM_PARENT_H_
+#endif  // PROJECT3_INC_SHM_PARENT_H_
