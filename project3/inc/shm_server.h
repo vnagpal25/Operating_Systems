0bf7f915fcdc4dfa5c06f42b048865c35887bc8b
Vansh Nagpal <you@example.com>
2023-04-04 01:19:44 -0400

    documentation/linting

--- project3/inc/shm_server.h
+++ project3/inc/shm_server.h
@@ -1,24 +1,60 @@
-#ifndef _SHM_SERVER_H_
-#define _SHM_SERVER_H_
+/*Copyright 2023 CSCE311 Project 3
+ * author - vnagpal
+ * Contains definition of server shared memory class
+ */
+#ifndef PROJECT3_INC_SHM_SERVER_H_
+#define PROJECT3_INC_SHM_SERVER_H_
 
-#include <cstring>
-#include <iostream>
-#include <string>
-#include <vector>
+#include <cstring>   // cstring functionality
+#include <iostream>  // IO
+#include <string>    // string functionality
+#include <vector>    // vector functinality
 
-#include "shm_parent.h"
+#include "shm_parent.h"  // parent functionality
+
+// for readability
 using std::string;
 class SharedMemoryServer : public SharedMemoryParent {
  public:
-  explicit SharedMemoryServer();
-  void RunServer();
+  /**
+   * Constructor for server, calls helper methods for semaphores
+   */
+  SharedMemoryServer();
+
+  /**
+   * Main run method, main flow of logic, calls helper methods
+   * @param print_welcome prevents "SERVER STARTED" from being printed each time
+   * a client connects
+   */
+  void RunServer(bool print_welcome);
 
  private:
-  void CreateSemaphores();
-  void DestroySemaphores();
-  void ProcessFile();
-  void Sleep();
+  // file to be searched
   string file_path_;
+
+  // from parent, struct pointer for mapping shared memory on server side
   shm_buf_* shm_map_;
+
+  /**
+   * Method creates semaphores for consumer and producer
+   */
+  void CreateSemaphores();
+
+  /**
+   * Method destroys semaphores for consumer and producer
+   */
+  void DestroySemaphores();
+
+  /**
+   * Opens the file for processing and writes contents to shared memory
+   */
+  void ProcessFile();
+
+  /**
+   * Puts Server to sleep, and wakes up client.
+   * Used after server is done processing processing part of the file so that
+   * the client can read it
+   */
+  void Sleep();
 };
-#endif  // _SHM_SERVER_H_
+#endif  // PROJECT3_INC_SHM_SERVER_H_
