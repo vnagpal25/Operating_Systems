4b767ff70f58190d6f4cd0e8850cd120b8fc7cf4
Vansh Nagpal <you@example.com>
2023-04-04 13:37:23 -0400

    linting/documentation

--- project3/src/main_server.cc
+++ project3/src/main_server.cc
@@ -1,11 +1,20 @@
-#include <cstring>
-#include <iostream>
+/*Copyright 2023 CSCE311 Project 3
+ * author - vnagpal
+ * Contains main method for server, makes/runs server instance
+ */
+#include <cstring>   // cstring functions
+#include <iostream>  // IO
 
-#include "project3/inc/shm_parent.h"
-#include "project3/inc/shm_server.h"
+#include "project3/inc/shm_parent.h"  // parent functionality
+#include "project3/inc/shm_server.h"  // server functionality
+
+// readability
 using std::cout, std::clog, std::endl;
 int main(int argc, char* argv[]) {
+  // creates/runs server instance
   SharedMemoryServer shm_server;
   shm_server.RunServer(true);
+
+  // exits
   return 0;
 }
